cd8dfca47c9205cf361c62a50d9fb9a482d80ac4
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-11 17:24:19 -0400

    Updating files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,23 @@
-var questions = ["Which is the key sign of an array?" , "Which is the correct syntax for a \"for\" loop?" , 
-    "Which of the following is a function?" , "Which of the following is a string?" , "What does \"5\" + 10 = ?"];
+// var questions = ["Which is the key sign of an array?" , "Which is the correct syntax for a \"for\" loop?" , 
+//     "Which of the following is a function?" , "Which of the following is a string?" , "What does \"5\" + 10 = ?"];
 
-var answers = ["[]" , "for (var i = 0; i < x; i++)" , "function()" , "Welcome to the beginging" , "510"];
+// var answers = ["[]" , "for (var i = 0; i < x; i++)" , "function()" , "Welcome to the beginging" , "510"];
 
-for (var i = 0; i < questions.length; i++) {
-    var question = document.getElementById("questions");
-    var questionEl = document.createElement("h2");
+// for (var i = 0; i < questions.length; i++) {
+//     var question = document.getElementById("questions");
+//     var questionEl = document.createElement("h2");
 
-    questionEl.textContent = questions[i];
-    question.innerHTML = questionEl;
-}
+//     questionEl.textContent = questions[i];
+//     question.innerHTML = questionEl;
+// }
+
+// Establishing elements to put onto the page 
+var h1El = document.createElement("h1");
+var pEl = document.createElement("p");
+var btnEl = document.createElement("button");
+
+h1El.textContent = "JavaScript Pop Quiz";
+pEl.textContent = "Try to answer the following javascript related questions before time runs out. Wrong answers will subtract 10 seconds of the clock. Good Luck!";
+btnEl.textContent = "Start Quiz";
 
  
