50ac3204b37b83f4fdd79dfcf28221d773dcfc02
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-13 23:44:07 -0400

    Adding files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,75 +6,44 @@ document.getElementById("startBtn").addEventListener("click" , function() {
     quiz();
 })
 
+
 //  Creating a function that displays and removes html elements based what user does
 function quiz() {
-        // Creating every element for quiz card 
-        var mainDivElement = document.createElement("div");
-        var h2EL = document.createElement("h2");
-        var orderedList = document.createElement("ol");
-        var li = document.createElement("li");
-        var li2 = document.createElement("li");
-        var li3 = document.createElement("li");
-        var li4 = document.createElement("li");
-        var btnEl1 = document.createElement("button");
-        var btnEl2 = document.createElement("button");
-        var btnEl3 = document.createElement("button");
-        var btnEl4 = document.createElement("button");
-        var brEl = document.createElement("br");
-        var hrEL = document.createElement("hr");
-        var pTag = document.createElement("p");
-
-        mainDivElement.setAttribute("id" , "main-card-questions");
-        mainDivElement.setAttribute("class" , "container mx-auto p-3 w-50");
-    
-        // Applying all the nescessary attribues for targeting/ styling
-        mainDivElement.setAttribute("id" , "main-card-questions");
-        mainDivElement.setAttribute("class" , "container mx-auto p-3 w-50");
-    
-        h2EL.setAttribute("id" , "questions");
-        h2EL.setAttribute("class" , "m-2 mb-3");
-    
-        orderedList.setAttribute("id" , "olEl");
-        orderedList.setAttribute("class" , "mr-auto");
-
-        li.setAttribute("id" , "option1");
-        li.setAttribute("class" , "m-2");
-        li2.setAttribute("id" , "option2");
-        li2.setAttribute("class" , "m-2");
-        li3.setAttribute("id" , "option3");
-        li3.setAttribute("class" , "m-2");
-        li4.setAttribute("id" , "option4");
-        li4.setAttribute("class" , "m-2");
+    // Creating, setting attributes ,and pushing main div into DOM
+    var brEl = document.createElement("br");
+    var hrEl = document.createElement("hr");
+    var mainDiv = document.createElement("div");
+    mainDiv.setAttribute("id" , "main-card-questions");
+    mainDiv.setAttribute("class" , "mx-auto p-3 w-50");
+    document.body.appendChild(mainDiv);
 
 
-        hrEL.setAttribute("class" , "line-break");
+    // Array to hold questions and answers
+    var questions = ["Which character defines an array?" , "Which is the correct syntax for a \"for\" loop?" , "Which of the following is a function?" , "Which of the following is a string?" , "What does \"5\" + 10 = ?"];
+    var mulitChoice = [["a.{}" , "b.()" , "c.[]" , "d.</>"], ["a.for (i=0+i<5+i++)" , "b.for (var i=0; i<5; i++)" , "c.for(vari=0 i<5 i++)" , "d.for {var i=0; i<5; i++)"],
+        ["a.function{}" , "b.function++" , "c.function.function" , "d.function()"], ["a.\"5\"" , "b.20" , "c.alert()" , "d.styles.css"], ["a.510" , "b.50" , "c.15" , "d.undefined"]];
+    var trueAnswers = ["c.[]" , "b.for (var i=0; i<5; i++)" , "d.function()" , "a.\"5\"" , "a.510"];
 
-        pTag.setAttribute("class" , "text-muted mr-auto pl-4");
+    for (var i = 0; i < questions.length; i++) {
+        console.log(i)
 
-        // Pushing main div to the page
-        document.body.appendChild(mainDivElement);
+    // Question 1
+        if (i < 1) {
+            var question = document.createElement("h2");
+            question.setAttribute("id" , "question");
+            question.setAttribute("class" , "m-2 mb-3");
+            document.getElementById("main-card-questions").appendChild(question);
 
-        // Adding children
-        document.getElementById("main-card-questions").appendChild(orderedList);
+            var questionDisplayed = document.getElementById("question");
+            questionDisplayed.textContent = questions[i];
 
-        document.getElementById("olEl").appendChild(li);
-        document.getElementById("option1").appendChild(btnEl1);
-
-        document.getElementById("olEl").appendChild(li2);
-        document.getElementById("option2").appendChild(btnEl2);
-
-        document.getElementById("olEl").appendChild(li3);
-        document.getElementById("option3").appendChild(btnEl3);
-
-        document.getElementById("olEl").appendChild(li4);
-        document.getElementById("option4").appendChild(btnEl4);
-
-        document.getElementById("main-card-questions").appendChild(brEl);
-        document.getElementById("main-card-questions").appendChild(hrEL);
-        document.getElementById("main-card-questions").appendChild(pTag);
-
-
-
-    
-
-}
+            for (var j = 0; j < 4; j++) {
+                var choicesBtns = document.createElement("button");
+                choicesBtns.setAttribute("id" , "choices");
+                choicesBtns.setAttribute("class" , "m-1 mr-auto");
+                document.getElementById("main-card-questions").appendChild(choicesBtns);
+                choicesBtns.textContent = mulitChoice[i][j];
+            }
+        }
+    }
+}     
