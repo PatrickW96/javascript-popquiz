a5d21135bcfbc12e8033ffb2a6a52dee0a387805
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-14 12:50:02 -0400

    Updating files. Still playing with code with the goal of developing the most efficient code, while meeting deadline.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,3 @@
-
 // Adding an event listener with a fucntion that starts the quiz
 document.getElementById("startBtn").addEventListener("click" , function() {
     // Removes the "main-card" div from the DOM 
@@ -6,44 +5,34 @@ document.getElementById("startBtn").addEventListener("click" , function() {
     quiz();
 })
 
-
-//  Creating a function that displays and removes html elements based what user does
-function quiz() {
-    // Creating, setting attributes ,and pushing main div into DOM
-    var brEl = document.createElement("br");
-    var hrEl = document.createElement("hr");
-    var mainDiv = document.createElement("div");
-    mainDiv.setAttribute("id" , "main-card-questions");
+function quizSetup() {
+    // Creating main div and submain children divs
+    var mainDiv =  document.createElement("div");
+    mainDiv.setAttribute("id" , "question-card");
     mainDiv.setAttribute("class" , "mx-auto p-3 w-50");
     document.body.appendChild(mainDiv);
 
+    var questionsDiv =  document.createElement("div");
+    questionsDiv.setAttribute("id" , "questions");
+    questionsDiv.setAttribute("class" , "container");
+    document.getElementById("question-card").appendChild(questionsDiv);
 
-    // Array to hold questions and answers
-    var questions = ["Which character defines an array?" , "Which is the correct syntax for a \"for\" loop?" , "Which of the following is a function?" , "Which of the following is a string?" , "What does \"5\" + 10 = ?"];
-    var mulitChoice = [["a.{}" , "b.()" , "c.[]" , "d.</>"], ["a.for (i=0+i<5+i++)" , "b.for (var i=0; i<5; i++)" , "c.for(vari=0 i<5 i++)" , "d.for {var i=0; i<5; i++)"],
-        ["a.function{}" , "b.function++" , "c.function.function" , "d.function()"], ["a.\"5\"" , "b.20" , "c.alert()" , "d.styles.css"], ["a.510" , "b.50" , "c.15" , "d.undefined"]];
-    var trueAnswers = ["c.[]" , "b.for (var i=0; i<5; i++)" , "d.function()" , "a.\"5\"" , "a.510"];
+    var gradeDiv =  document.createElement("div");
+    gradeDiv.setAttribute("id" , "answers");
+    gradeDiv.setAttribute("class" , "container");
+    document.getElementById("question-card").appendChild(gradeDiv);
 
-    for (var i = 0; i < questions.length; i++) {
-        console.log(i)
 
-    // Question 1
-        if (i < 1) {
-            var question = document.createElement("h2");
-            question.setAttribute("id" , "question");
-            question.setAttribute("class" , "m-2 mb-3");
-            document.getElementById("main-card-questions").appendChild(question);
+}
 
-            var questionDisplayed = document.getElementById("question");
-            questionDisplayed.textContent = questions[i];
+function displayQuiz (
 
-            for (var j = 0; j < 4; j++) {
-                var choicesBtns = document.createElement("button");
-                choicesBtns.setAttribute("id" , "choices");
-                choicesBtns.setAttribute("class" , "m-1 mr-auto");
-                document.getElementById("main-card-questions").appendChild(choicesBtns);
-                choicesBtns.textContent = mulitChoice[i][j];
-            }
-        }
-    }
-}     
+)
+
+
+
+
+ 
+
+
+
