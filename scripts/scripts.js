d9541047707606759598bd2227fa9923dca2fca1
Patrick W <pat.realrealestatellc@gmail.com>
2020-10-05 20:10:12 -0400

    Updating style and script files to make user interface a better experience.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,7 +27,9 @@ var cardContent = [
 ];
 
 var c = 0;
-var userScore = 0;
+var seconds = 50;
+var score = 0;
+var setTimer;
 
 // Adding an event listener with a fucntion that starts the quiz
 document.getElementById("startBtn").addEventListener("click" , function() {
@@ -37,10 +39,10 @@ document.getElementById("startBtn").addEventListener("click" , function() {
         // That way we can resuse the code. 
         // Basically we make sure the whole card is deleted before start adding the new questions
     createQuestionTemplate();
-
     displayContent();
-})
-
+    setTimer = setInterval(quizTimer , 1000);
+    quizTimer();
+});
 
 // Laying out all the HTML that will be used for the quiz
 function createQuestionTemplate() {
@@ -83,7 +85,7 @@ function createQuestionTemplate() {
     gradeEL.setAttribute("id" , "question-grade");
     gradeEL.setAttribute("class" , "text-muted pl-4  mr-auto grade");
     document.getElementById("question-card").appendChild(gradeEL);
-}
+};
 
 
 // Function to display content based on conditions 
@@ -94,7 +96,7 @@ function displayContent() {
 
     if (c > 4) {
         endQuiz();
-    }
+    };
 
     var question = cardContent[c].question;
     var questionDisplayed = document.getElementById("question-display");
@@ -109,27 +111,30 @@ function displayContent() {
             event.stopPropagation();
             handleClick(event);
             })
-        }  
-}
+        };  
+};
 
 // Function for click events that take place during the quiz
 function handleClick(event) {
     console.log('Current: ', c)
     console.log(cardContent[1].answer)
+
+    var grade = document.getElementById("question-grade");
     var targetClick = event.target.textContent;
     console.log(targetClick);
 
     if (targetClick !== cardContent[c].answer) {
-        var grade = document.getElementById("question-grade");
+        document.getElementById('question-grade').classList.add('alert-danger')
         grade.textContent = "Incorrect.";
+        seconds -= 10;
         goToNext();
-    } else {
-        var grade = document.getElementById("question-grade");
+    } else if (targetClick == cardContent[c].answer) {
+        document.getElementById('question-grade').classList.add('alert-success')
         grade.textContent = "Correct.";
-        userScore + 100;
+        score += 100;
         goToNext();
-    } 
-}
+    };
+};
 
 
 function goToNext() {
@@ -137,13 +142,14 @@ function goToNext() {
     setTimeout(function() {
         displayContent();
     }, 500)
-}
+};
 
 function endQuiz() {
+    clearInterval(setTimer);
     document.getElementById("question-card").remove();
     endGameTemplate();
     endTemplateDisplay();
-}
+};
 
 
 function endGameTemplate() {
@@ -176,7 +182,7 @@ function endGameTemplate() {
     document.getElementById("form").appendChild(inputEL);
     // Nesting button in formEL
     var sumbitBtn =  document.createElement("button");
-    sumbitBtn.setAttribute("id" , "submit-button");
+    sumbitBtn.setAttribute("id" , "submitBtn");
     sumbitBtn.setAttribute("class" , "col mx-3 p-0 submit");
     document.getElementById("form").appendChild(sumbitBtn);
     // Line break
@@ -186,27 +192,89 @@ function endGameTemplate() {
     var hrEl = document.createElement("hr");
     hrEl.setAttribute("class" , "line-break2");
     document.getElementById("end-card").appendChild(hrEl);  
-}
+};
 
 function endTemplateDisplay() {
     var allDone = document.querySelector(".h1-tag");
     allDone.textContent = "All Done!"
 
     var pTag = document.querySelector(".p-tag");
-    pTag.textContent = "Your Final Score is : " + userScore + " !!!";
+    pTag.textContent = "Your Final Score is : " + score + " !!!";
 
     var labelTag = document.querySelector(".label-tag");
-    labelTag.textContent = "Enter Initials";
+    labelTag.textContent = "Enter Initials: ";
 
     var buttonTag = document.querySelector(".submit");
     buttonTag.textContent = "Submit";
 
-    buttonTag.addEventListener("click" , btnClick());
-}
+    document.querySelector('.submit').addEventListener('click' , btnClick);
+};
 
 function btnClick() {
-    var userInitials = document.querySelector(".input-tag");
-    localStorage.setItem("initials", userInitials.innerHTML);
-    // var storeScore = localStorage.setItem("score" , userScore);
+    document.getElementById("end-card").remove();
 
-}
+    displayhighScoreTemplate();
+    setTimeout(5000);
+};
+
+function displayhighScoreTemplate() {
+    var divContainer = document.createElement('div');
+    divContainer.setAttribute('id' , 'highScoreDiv');
+    divContainer.setAttribute('class' , 'container p-3 w-50');
+    document.body.append(divContainer);
+
+    var highScore = document.createElement('h2');
+    highScore.setAttribute('class' , 'highScoreText');
+    document.getElementById('highScoreDiv').append(highScore);
+
+    var listScore = document.createElement('ul');
+    listScore.setAttribute('id' , 'highScoreList');
+    listScore.setAttribute('class' , 'container p-3 w-50');
+    document.getElementById('highScoreDiv').append(listScore);
+
+    var listedItem = document.createElement('li');
+    listedItem.setAttribute('id' , 'userScore');
+
+    document.querySelector('.highScoreText').textContent = "HIGH SCORE";
+};
+
+// function reCreateMain() {
+//     document.getElementById("highScoreDiv").remove();
+
+//     var container = document.createElement('div');
+//     container.setAttribute('id' , 'main-card');
+//     container.setAttribute('class' , 'container mx-auto p-3 w-50 text-center');
+//     document.body.append(container);
+
+//     var title = document.createElement('h1');
+//     title.setAttribute('class' , 'm-2');
+//     title.textContent = "Javascript Pop Quiz";
+//     document.getElementById('main-card').append(title);
+
+//     var paragraph = document.createElement('p');
+//     paragraph.setAttribute('class' , 'm-2');
+//     paragraph.textContent = "Try to answer the following javascript related questions before time runs out. Wrong answers will subtract 10 seconds of the clock. Good Luck!";
+//     document.getElementById('main-card').append(paragraph);
+
+
+//     var btn = document.createElement('button');
+//     btn.setAttribute('id' , 'startBtn');
+//     btn.setAttribute('class' , 'm-2');
+//     btn.textContent = "Start Quiz";
+//     document.getElementById('main-card').append(btn);
+
+//     seconds = 50;
+//     timer = document.getElementById('timer');
+//     timer.textContent = seconds;
+// };
+
+function quizTimer() {
+    timer = document.getElementById('timer');
+    seconds--;
+    timer.textContent = seconds;
+
+    if (seconds == 0) {
+        clearInterval(setTimer);
+        endQuiz();
+    }; 
+};
